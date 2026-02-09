f1a51036d5f5f82902a05e551ef72b54934d65f7
MaxEnergetic <stardubmaxedu@gmail.com>
2026-02-09 10:43:31 +0300

    blinking script

---
+++ get_led/digital-blink.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+
+led=26
+
+gpio.setup(led, gpio.OUT)
+
+state=0
+period = 0.5
+i=0
+while True:
+    state = not state 
+    gpio.output(led,state)
+    time.sleep(period)
+    print(i)
+    i+=0.5
