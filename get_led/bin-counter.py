adcea48b9bfe6acab587910c7fbee91a7fd96d0d
MaxEnergetic <stardubmaxedu@gmail.com>
2026-02-15 14:55:04 +0300

    fixed

--- get_led/bin-counter.py
+++ get_led/bin-counter.py
@@ -2,27 +2,33 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BCM)
-leds = [24, 22, 23, 27, 17, 25, 5, 16]
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
 gpio.setup(leds, gpio.OUT)
+gpio.output(leds, 0)
+
+up = 1
+down = 2
+gpio.setup([up, down], gpio.IN)
+
 num = 0
-up=1
-down=2
-gpio.setup([up,down], gpio.IN)
 
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
-sleep_time = 0.2
-while True:
-    if gpio.input(up)>0:
-        if num +1 <= 7:
-            num+=1
-        print(num, dec2bin(num))
-        time.sleep(sleep_time)
-        GPIO.output(leds, dec2bin(num))
 
-    if gpio.input(down)>0:
-        if num +1 >= 0:
-            num-=1
+sleep_time = 0.2
+
+while True:
+    if gpio.input(up):
+        if num < 255:
+            num += 1
         print(num, dec2bin(num))
+        gpio.output(leds, dec2bin(num))
         time.sleep(sleep_time)
-        GPIO.output(leds, dec2bin(num))
+
+    if gpio.input(down):
+        if num > 0:
+            num -= 1
+        print(num, dec2bin(num))
+        gpio.output(leds, dec2bin(num))
+        time.sleep(sleep_time)
