543a32e47f740dbd3235d397eb9f2fb0f5059985
MaxEnergetic <stardubmaxedu@gmail.com>
2026-02-16 12:02:09 +0300

    a

--- get_dac/r2r-sin.py
+++ get_dac/r2r-sin.py
@@ -7,8 +7,10 @@ signal_frequency = 10
 sampling_frequency = 1000
 
 
-class R2R_DAC:
+import RPi.GPIO as GPIO
+import time
 
+class R2R_DAC:
     def __init__(self, gpio_bits, dynamic_range, verbose = False):
         self.gpio_bits = gpio_bits
         self.dynamic_range = dynamic_range
@@ -33,10 +35,16 @@ class R2R_DAC:
 
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
-            print(f"Напряжение выходит за диапазон (0 - {self.dynamic_range:.2f} В)")
-            print("Устанавливаем 0 В")
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавливаем 0.0 В")
             voltage = 0.0
+
         number = int(voltage / self.dynamic_range * 255)
+        self.set_number(number)
+
+        if self.verbose:
+            bits = [int(bit) for bit in bin(number)[2:].zfill(8)]
+            print(f"Число на вход ЦАП: {number}, биты: {bits}")
 try:
     dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
     dynamic_range = 3.183
